d7a520a4e5dfcc0e956c508ec462a2397ce38fed
Jordan <jordancondipodero@hotmail.com>
2022-09-04 23:45:29 +0200

    Basic movement input
    Adding basic movement
    
    Jordan

--- Source/deadlymaze_ue5/Public/SCharacter.h
+++ Source/deadlymaze_ue5/Public/SCharacter.h
@@ -30,6 +30,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void MoveForward(float value);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
