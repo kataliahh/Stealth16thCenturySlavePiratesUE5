a8fd3fe2d6bd4723e571608deb477f6d82db536c
Alek <llUntamed32ll@gmail.com>
2022-09-13 18:29:55 +0430

    Vanishing/Appearing objects added.
    **I have implemented Vanishing/Appearing Objects functionality.
    (this magic works by using "Q").
    ** Added a BaseClass for all the objects that we can use magic on called "MagicalActor" **
    
    Co-Authored-By: Alek <alek.br@gmail.com>

--- Source/StealthGame/MagicSystem/MagicComponent.h
+++ Source/StealthGame/MagicSystem/MagicComponent.h
@@ -7,8 +7,7 @@
 #include "Components/TimelineComponent.h"
 #include "MagicComponent.generated.h"
 
-
-struct FTimeline;
+class AMagicalActor;
 //this component is used to implement any kind of magic ability that can be cast.
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class STEALTHGAME_API UMagicComponent : public UActorComponent
@@ -22,13 +21,28 @@ public:
 
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-	void setLevitatingValues(AActor* ActorToLevitate, const FVector& LiftLocation);
-	AActor* findObjectToLevitate(class ACharacterBase* PC);
 
-protected:
+	//LEVITATING-SYSTEM FUNCTIONS.
+	void setLevitatingValues(AMagicalActor* ActorToLevitate, const FVector& LiftLocation);
 	
+	//END OF LEVITATING-SYSTEM FUNCTIONS.
+
+
+	//VANISHING-SYSTEM FUNCTIONS.
+
+	//used to vanish/appear objects.
+	bool setActorVisibility(AMagicalActor* MagicalActor);
+
+	//VANISHING-SYSTEM FUNCTIONS.
+
+	//used in different magic systems so we don't place it under any system comments.
+	AMagicalActor* lineTraceFromCamera(class ACharacterBase* PC);
+protected:
+
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	//LEVITATING-MAGIC PROPERTIES.
 
 	UPROPERTY(EditDefaultsOnly, Category = LevitatingMagic)
 		///used to see if there is an object to levitate within the range of player's viewpoint.
@@ -41,18 +55,37 @@ protected:
 		float m_LevitateSpeed{ 100.f };
 
 	bool m_bCanLevitate{ false };
+	//used to check if the levitated obj has reached the top point of levitation, so we can add jiggling movement to it.
+	bool m_bReachedPeakPoint{ false };
 
 	FTimeline m_LevitatingTimeline;
-	FVector m_LevitatingLocation{};
-	
-	//UPROPERTY()
-		AActor* m_ActorToLevitate;
 
-	//this function is called through the LiftingTimeline Delegate.
+	FVector m_LevitatingLocation{};
+
+	FVector m_PeakLocation{};
+
+	UPROPERTY(EditDefaultsOnly)
+		float m_JiggleHeight{ 1500.f };
+
+	UPROPERTY(EditDefaultsOnly)
+		float m_JiggleMoveSpeed{ 5.f };
+
+	UPROPERTY()
+	AActor* m_ActorToLevitate{ nullptr };
+
+	class UStaticMeshComponent* m_LevitatedActorMesh{ nullptr };
+	//this function is called through the LevitatingTimeline Delegate.
 	UFUNCTION()
-		void LevitateOverTime(float Value);
+		void levitateOverTime(float Value);
+
+	void jiggleLevitatedObject();
 
 	void dropObject();
+	//END OF LEVITATING PROPERTIES.
+
+
+	
+
 
 
 
